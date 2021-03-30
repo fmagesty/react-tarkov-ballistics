ca4c116b9a6fc2ab43a7d6c08e835ec282c6165b
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-03-30 11:41:47 -0300

    adicionados suporte ao componente CaliberDropdown.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import CaliberDropdown from './CaliberDropdown.js';
+import tarkovImage from './images/tarkovBodyParts.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <h1>Escape From Tarkov Ballistics Simulator</h1>
+      <CaliberDropdown />
+      <img src={tarkovImage} alt="" id="tarkov-img"/>
     </div>
   );
 }
